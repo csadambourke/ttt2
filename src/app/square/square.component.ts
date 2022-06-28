cd3415e011a19d1b734b31d239b037c98bfb6876
Adam Bourke <csadambourke@gmail.com>
2022-06-29 08:46:06 +1000

    Fixed up square sizing and styling

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: ` <button>{{ value }}</button> `,
-  styles: [],
+  styleUrls: ['./square.component.scss'],
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
